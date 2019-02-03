e626d17fe3078b30e6230fe85e94030b339f03b6
清水幸佑 <thimi0412@icloud.com>
2019-02-03 22:43:48 +0900

    return json

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
+
+type User struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func userHandler(w http.ResponseWriter, r *http.Request) {
+	user := User{
+		1, "Kosuke",
+	}
+	res, err := json.Marshal(user)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(res)
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World")
@@ -11,5 +32,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/user", userHandler)
 	http.ListenAndServe(":8080", nil)
 }
